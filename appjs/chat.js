b9edb41c730bf54e035a5480a1e42a4a97cfdcc1
Kenneth Padro <kenneth.padro@upr.edu>
2019-04-08 23:51:35 -0400

    Update chat.js
    UNA CABRONA X ME ASUSTO

--- appjs/chat.js
+++ appjs/chat.js
@@ -1,4 +1,4 @@
-xangular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope', '$location', '$route', '$routeParams', '$localStorage',
+angular.module('AppChat').controller('ChatController', ['$http', '$log', '$scope', '$location', '$route', '$routeParams', '$localStorage',
     function ($http, $log, $scope, $location, $route, $routeParams, $localStorage) {
         var thisCtrl = this;
         this.gID = $routeParams.gID;
